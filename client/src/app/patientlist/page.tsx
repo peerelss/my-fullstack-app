593268cd59da6989a06f7ef6f3c1b80d7385ec19
kevin <xiepeerless@gmail.com>
2024-10-26 09:22:51 -0700

    add knex db file

--- client/src/app/patientlist/page.tsx
+++ client/src/app/patientlist/page.tsx
@@ -2,10 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './page.css';
-
+import Link from 'next/link';
 const PatientList = () => {
   const [patients, setPatients] = useState([]);
-
   // 获取病人数据
   useEffect(() => {
     const fetchPatients = async () => {
@@ -19,7 +18,9 @@ const PatientList = () => {
 
     fetchPatients();
   }, []);
-
+  const handleEdit = (id) => {
+   // router.push(`/EditPatient?id=${id}`);
+  };
   return (
     <div className="patient-list">
       <h2>Patient List</h2>
@@ -28,27 +29,33 @@ const PatientList = () => {
       ) : (
         <table>
           <thead>
-            <tr>
-              <th>Patient ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Phone</th>
-              <th>DOB</th>
-              <th>Address</th>
-              <th>Gender</th>
-            </tr>
+          <tr>
+            <th>Patient ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Phone</th>
+            <th>DOB</th>
+            <th>Address</th>
+            <th>Gender</th>
+            <th>edit</th>
+          </tr>
           </thead>
           <tbody>
             {patients.map((patient) => (
-              <tr key={patient.id}>
-                <td>{patient.patient_id}</td>
-                <td>{patient.first_name}</td>
-                <td>{patient.last_name}</td>
-                <td>{patient.phone}</td>
-                <td>{patient.DOB}</td>
-                <td>{patient.address}</td>
-                <td>{patient.gender}</td>
-              </tr>
+                <tr key={patient.id}>
+                  <td>{patient.patient_id}</td>
+                  <td>{patient.first_name}</td>
+                  <td>{patient.last_name}</td>
+                  <td>{patient.phone}</td>
+                  <td>{patient.DOB}</td>
+                  <td>{patient.address}</td>
+                  <td>{patient.gender}</td>
+                  <td>
+                    <Link href={`/EditPatient?id=${patient.id}`}>
+                      <button>编辑</button>
+                    </Link>
+                  </td>
+                </tr>
             ))}
           </tbody>
         </table>
